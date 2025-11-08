228ee2d1fad1da30f2662a47c133c092fefbb27c
Sunny <79081866+SunnyNoob132@users.noreply.github.com>
2025-11-08 14:01:43 +1300

    Heheheha

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -7,7 +7,7 @@ return {
             current_line_blame = true
         }
     },
-    --[[{
+    {
         "NeogitOrg/neogit",
 
         dependencies = {
@@ -15,25 +15,5 @@ return {
             "sindrets/diffview.nvim",
             "nvim-telescope/telescope.nvim"
         },
-    }]]
-    {
-        "kdheepak/lazygit.nvim",
-        lazy = true,
-        cmd = {
-            "LazyGit",
-            "LazyGitConfig",
-            "LazyGitCurrentFile",
-            "LazyGitFilter",
-            "LazyGitFilterCurrentFile",
-        },
-        -- optional for floating window border decoration
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-        },
-        -- setting the keybinding for LazyGit with 'keys' is recommended in
-        -- order to load the plugin when the command is run for the first time
-        keys = {
-            { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
-        }
     }
 }
