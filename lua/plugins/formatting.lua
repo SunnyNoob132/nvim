cfaca3ceba930744ddc0cd3e1873101f6e1d8b19
Sunny <79081866+SunnyNoob132@users.noreply.github.com>
2025-11-08 18:19:06 +1300

    Added todo comments

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -93,5 +93,14 @@ return {
 
             hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
         end
+    },
+    {
+        "folke/todo-comments.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        opts = {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+        }
     }
 }
