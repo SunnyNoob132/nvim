d891d60babfbb1cd4fbe1304d3a1f325072b1c7c
Sunny <79081866+SunnyNoob132@users.noreply.github.com>
2026-02-26 22:57:56 +1100

    Added the ability to view various image and document files in neovim

---
+++ lua/plugins/visuals.lua
@@ -0,0 +1,41 @@
+return {
+    {
+        "brenoprata10/nvim-highlight-colors",
+        event = { "BufReadPost", "BufWritePost", "BufNewFile" },
+
+        opts = {
+            render = "virtual",
+            virtual_symbol = "󱓻",
+            enable_tailwind = true,
+            exclude_filetypes = { "lazy" },
+        },
+    },
+
+    {
+        "nvzone/minty",
+        cmd = { "Shades", "Huefy" },
+    },
+
+    {
+        "3rd/image.nvim",
+        build = false,
+
+        opts = {
+            backend = "kitty",
+            processor = "magick_cli",
+        },
+    },
+    {
+        "brianhuster/live-preview.nvim",
+
+        dependencies = {
+            "nvim-telescope/telescope.nvim",
+        },
+
+        config = function()
+            require("livepreview.config").set({
+                dynamic_root = true,
+            })
+        end,
+    },
+}
