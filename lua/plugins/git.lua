f1473c49b391909ee4ddf388fb5e3f5c7dc1f3ef
Sunny <79081866+SunnyNoob132@users.noreply.github.com>
2026-06-11 21:10:45 +1000

    Added a Neogit shortcut

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -17,5 +17,9 @@ return {
             "sindrets/diffview.nvim",
             "nvim-telescope/telescope.nvim",
         },
+
+        keys = {
+            { "<leader>gg", "<cmd>Neogit<cr>", desc = "Show Neogit UI" },
+        },
     },
 }
