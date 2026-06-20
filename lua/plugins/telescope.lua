ef877fa357c7545e63927d3753a076c49c14d60b
Sunny <79081866+SunnyNoob132@users.noreply.github.com>
2026-06-20 13:38:11 +1000

    feat: test
    test
    
    test

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -12,11 +12,42 @@ return {
                     override_file_sorter = true,
                     case_mode = "smart_case",
                 },
+
+                conventional_commits = {
+                    action = function(entry)
+                        vim.print(entry)
+                    end,
+
+                    include_body_and_footer = true,
+                },
             },
         },
     },
     {
         "nvim-telescope/telescope-fzf-native.nvim",
         build = "make",
+    },
+    {
+        "olacin/telescope-cc.nvim",
+        dependencies = { "nvim-telescope/telescope.nvim" },
+
+        config = function()
+            vim.api.nvim_create_autocmd("FileType", {
+                pattern = "gitcommit",
+                callback = function()
+                    vim.keymap.set("n", "<leader>cc", function()
+                        local actions = require("telescope._extensions.conventional_commits.actions")
+                        local picker = require("telescope._extensions.conventional_commits.picker")
+
+                        local opts = {
+                            action = actions.prompt,
+                            include_body_and_footer = true,
+                        }
+
+                        picker(opts)
+                    end, { buffer = true, desc = "Create conventional commit message" })
+                end,
+            })
+        end,
     },
 }
