07de941009a81321eac2fde980427d884960aa1a
Sunny <79081866+SunnyNoob132@users.noreply.github.com>
2026-07-09 13:18:45 +1000

    revert: removed conventional commits telescope plugin

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -12,39 +12,11 @@ return {
                     override_file_sorter = true,
                     case_mode = "smart_case",
                 },
-
-                conventional_commits = {
-                    action = function(entry)
-                        vim.print(entry)
-                    end,
-                },
             },
         },
     },
     {
         "nvim-telescope/telescope-fzf-native.nvim",
         build = "make",
-    },
-    {
-        "olacin/telescope-cc.nvim",
-        dependencies = { "nvim-telescope/telescope.nvim" },
-
-        config = function()
-            vim.api.nvim_create_autocmd("FileType", {
-                pattern = "gitcommit",
-                callback = function()
-                    vim.keymap.set("n", "<leader>cc", function()
-                        local actions = require("telescope._extensions.conventional_commits.actions")
-                        local picker = require("telescope._extensions.conventional_commits.picker")
-
-                        local opts = {
-                            action = actions.prompt,
-                        }
-
-                        picker(opts)
-                    end, { buffer = true, desc = "Create conventional commit message" })
-                end,
-            })
-        end,
     },
 }
