c09d803cd87b20ecbd51dc58c2b5a98d78fb8ba5
Sunny <79081866+SunnyNoob132@users.noreply.github.com>
2026-08-22 20:38:35 +1000

    autoindent on up or down

--- lua/config/binds.lua
+++ lua/config/binds.lua
@@ -10,3 +10,26 @@ vim.keymap.set("n", "<D-Right>", "<C-w>l", { desc = "Focus right window" })
 vim.keymap.set("n", "<leader>ll", "<cmd>Lazy<cr>", { desc = "Lazy UI" })
 vim.keymap.set("n", "<leader>ls", "<cmd>Lazy sync<cr>", { desc = "Lazy sync" })
 vim.keymap.set("n", "<leader>lu", "<cmd>Lazy update<cr>", { desc = "Lazy update" })
+
+-- Keep indentation when pressing return in insert
+local function smart_move(key)
+    return function()
+        local curr_line = vim.fn.line(".")
+        local target_line = key == "<Up>" and curr_line - 1 or curr_line + 1
+
+        if target_line < 1 or target_line > vim.fn.line("$") then
+            return key
+        end
+
+        local line_content = vim.fn.getline(target_line)
+
+        if line_content:match("^%s*$") then -- Check if the target line is empty
+            return key .. "<C-f>"
+        end
+
+        return key
+    end
+end
+
+vim.keymap.set("i", "<Up>", smart_move("<Up>"), { expr = true })
+vim.keymap.set("i", "<Down>", smart_move("<Down>"), { expr = true })
