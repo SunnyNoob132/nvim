4038a7587a8e9fe28c9c3ccca15dae3976da7bfc
Sunny <79081866+SunnyNoob132@users.noreply.github.com>
2026-08-26 15:43:03 +1000

    added telescope keybinds

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,6 +4,12 @@ return {
         dependencies = { "nvim-lua/plenary.nvim" },
         module = "telescope",
 
+        keys = {
+            { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
+            { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live Grep" },
+            { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Find Buffers" },
+        },
+
         opts = {
             extensions = {
                 fzf = {
