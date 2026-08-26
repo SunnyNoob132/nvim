4f9ecf4ab217b85919270de8a70a56bd42e07c14
Sunny <79081866+SunnyNoob132@users.noreply.github.com>
2026-08-26 15:52:43 +1000

    improved neotree ux

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -12,6 +12,11 @@ return {
 
         opts = {
             close_if_last_window = true,
+            use_libuv_file_watcher = true,
+
+            follow_current_file = {
+                enabled = true,
+            },
 
             filesystem = {
                 commands = {
